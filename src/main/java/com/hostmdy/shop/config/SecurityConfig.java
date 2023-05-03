691b94dfb6134e9b9b53fbe8e342d0d52a6b3249
JavaJobReadyBatch2 <javajobready.batch2@gmail.com>
2023-05-03 20:08:43 +0630

    finished basic auth

--- src/main/java/com/hostmdy/shop/config/SecurityConfig.java
+++ src/main/java/com/hostmdy/shop/config/SecurityConfig.java
@@ -3,8 +3,11 @@ package com.hostmdy.shop.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 import com.hostmdy.shop.service.impl.CustomUserDetailsService;
 
@@ -16,14 +19,39 @@ public class SecurityConfig {
 	
 	private final CustomUserDetailsService customUserDetailsService;
 	
+	private static final String[] PUBLIC_MATCHERS = {
+			"/api/user/**"
+	};
+	
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		http
+			.cors().disable().csrf().disable()
+			.authorizeHttpRequests()
+			.requestMatchers(PUBLIC_MATCHERS).permitAll()
+			.anyRequest().authenticated();
+		http.authenticationProvider(daoProvider());
+			
+		return http.build();
+		
+	}
+	
 	@Bean
 	public BCryptPasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
 	}
 	
 	@Bean
-	public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) {
-
+	public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception {
+		return authConfig.getAuthenticationManager();
+	}
+	
+	@Bean
+	public DaoAuthenticationProvider daoProvider() {
+		DaoAuthenticationProvider daoProvider = new DaoAuthenticationProvider();
+		daoProvider.setUserDetailsService(customUserDetailsService);
+		daoProvider.setPasswordEncoder(passwordEncoder());
+		return daoProvider;
 	}
 
 }
